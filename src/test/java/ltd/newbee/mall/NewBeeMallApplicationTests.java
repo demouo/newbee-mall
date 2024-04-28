214b22097a61103006b26bbe18f215e44c91334a
demouo <2081510953@qq.com>
2024-04-28 17:10:31 +0800

    Update 更新测试模板代码

--- src/test/java/ltd/newbee/mall/NewBeeMallApplicationTests.java
+++ src/test/java/ltd/newbee/mall/NewBeeMallApplicationTests.java
@@ -1,27 +1,39 @@
 package ltd.newbee.mall;
 
-import net.minidev.json.JSONObject;
+import ltd.newbee.mall.dao.MallUserMapper;
+import ltd.newbee.mall.entity.MallUser;
+import ltd.newbee.mall.service.NewBeeMallUserService;
+import ltd.newbee.mall.util.MD5Util;
+import org.apache.catalina.session.StandardManager;
+import org.apache.catalina.session.StandardSession;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.Scheduled;
 @SpringBootTest
 class NewBeeMallApplicationTests {
+    @Autowired
+    private MallUserMapper mallUserMapper;
+    @Autowired
+    private NewBeeMallUserService newBeeMallUserService;
 
     @Test
-//    @Scheduled(cron = "0/30 * * * * ?")
-    void testPost(){
-        // url
-        String url="http://localhost:28089/login";
-
-        // 造json对象
-        JSONObject body = new JSONObject();
-        body.put("account","");
-        body.put("password","");
-
-        // ......
-
-        // 打印信息
-        System.out.println("hello");
+    void testDao(){
+        MallUser mallUser = mallUserMapper.selectByLoginNameAndPasswd("", MD5Util.MD5Encode("", "UTF-8"));
+        System.out.println(mallUser);
     }
 
+    @Test
+    void testService(){
+        String resp = newBeeMallUserService.login("", MD5Util.MD5Encode("", "UTF-8"), new StandardSession(new StandardManager()));
+        System.out.println(resp);
+    }
+
+    /**
+     * 这个只能在postman、eolink、前端测试（按F12查看network测试，不然测不了）
+     */
+    @Test
+    void testController(){
+
+    }
 }
